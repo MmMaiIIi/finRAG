750c99ca3465e0c44d98bed5c0e5d16af81f0cda
MmMaiIIi <2360655509@qq.com>
2026-03-16 20:48:31 +0800

    rewrite

--- src/__init__.py
+++
@@ -1,2 +0,0 @@
-"""finRAG source package."""
-
--- src/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Shared utility modules."""
-
-from .config import load_yaml_config
-from .io import read_jsonl, write_jsonl
-from .logger import setup_logger
-
-__all__ = ["load_yaml_config", "read_jsonl", "write_jsonl", "setup_logger"]
-
